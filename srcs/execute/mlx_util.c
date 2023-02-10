85dbd17ad36ec8c618a18a3e4d75d704e833939d
Clearsu <clearwater1219@gmail.com>
2023-02-10 23:44:58 +0900

    Feat: my_mlx_pixel_put()

--- srcs/execute/mlx_util.c
+++ srcs/execute/mlx_util.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 22:56:57 by jincpark          #+#    #+#             */
-/*   Updated: 2023/02/10 23:36:00 by jincpark         ###   ########.fr       */
+/*   Updated: 2023/02/10 23:44:09 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,16 @@ void	init_mlx_and_img(t_mlx_vars *mlx_vars)
 	mlx_vars->img_data.addr = mlx_get_data_addr(mlx_vars->img_data.img,
 			&mlx_vars->img_data.bits_per_pixel,
 			&mlx_vars->img_data.line_length, &mlx_vars->img_data.endian);
+}
+
+void	my_mlx_pixel_put(t_img_data *img_data,
+		size_t x, size_t y, unsigned int color)
+{
+	char	*dest;
+
+	dest = img_data->addr 
+		+ (y * img_data->line_length + x * (img_data->bits_per_pixel / 8));
+	*(unsigned int *)dest = color;
 }
 
 int	key_hook_handler(int keycode, t_mlx_vars *mlx_vars)
