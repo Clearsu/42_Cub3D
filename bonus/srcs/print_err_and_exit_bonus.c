eb0f6c46741235ec2174d678fbce249f5f5bf430
Clearsu <clearwater1219@gmail.com>
2023-03-04 19:52:09 +0900

    Rename: moved to upper dir

---
+++ bonus/srcs/print_err_and_exit_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 17:22:53 by jincpark          #+#    #+#             */
+/*   Updated: 2023/03/04 19:46:51 by jincpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+#include "define_bonus.h"
+
+void	print_err_and_exit(t_err_type type)
+{
+	if (type == E_ARG_NUM)
+		ft_putstr_fd("cub3d: put at least or only one argument: Error\n", \
+				STDERR_FILENO);
+	else if (type == E_ARG_EXTENSION)
+		ft_putstr_fd("cub3d: invalid file type: Error\n", STDERR_FILENO);
+	else if (type == E_MAP)
+		ft_putstr_fd("cub3d: invalid map: Error\n", STDERR_FILENO);
+	else
+		perror("cub3d");
+	exit(1);
+}
+
+void	free_all(t_raycast_param *raycast_param)
+{
+	
+}
